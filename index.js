f58ca6d3785e5066baf18ec5ad0c767da9470d7b
Yuvraj Singh Rathore <141350177+rathoreyuvraj19@users.noreply.github.com>
2023-09-12 22:49:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+var buttonColor = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var level = 0;
+
+$("body").keydown(function () {
+  if (level == 0) {
+    nextSequence();
+  }
+});
+
+$(".button").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  check(userClickedPattern.length - 1);
+});
+
+function check(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("h1").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startover();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColor[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("." + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColour) {
+  $("." + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function startover() {
+  level = 0;
+  gamePattern = [];
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
